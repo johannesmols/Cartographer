8afa01a08ff143568faea3d5bddddb5fbf732927
Nebay <ricardoyaben@hotmail.es>
2018-04-17 11:59:30 +0200

    ///Documentation///
    To be added:
    * Buttons
    * More Overlays
    * Options
    * Date

--- app/src/main/java/itcom/cartographer/HeatmapActivity.java
+++ app/src/main/java/itcom/cartographer/HeatmapActivity.java
@@ -9,7 +9,6 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.TileOverlay;
 import com.google.android.gms.maps.model.TileOverlayOptions;
 import com.google.maps.android.heatmaps.Gradient;
@@ -51,20 +50,24 @@ public class HeatmapActivity extends FragmentActivity implements OnMapReadyCallb
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
+     *
+     * Please be sure you are using a device which can use google play services.
      */
 
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        //set the camera in Copenhagen
         LatLng copenhagen = new LatLng(55.650498, 12.555349);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(copenhagen));
+        //add the heat map Tile Overlay on top of the map
         addHeatMap();
     }
     private void addHeatMap(){
         // Create the gradient.
         int[] colors = {
-                Color.argb(0, 0, 255, 255),// transparent
+                Color.argb(0, 0, 255, 255),// transparent (alpha determines opacity)
                 Color.argb(255 / 3 * 2, 0, 255, 255), //soft blue
                 Color.rgb(0, 191, 255), //sky blue
                 Color.rgb(0, 0, 127), //deep blue
@@ -86,6 +89,7 @@ public class HeatmapActivity extends FragmentActivity implements OnMapReadyCallb
             // Add a tile overlay to the map, using the heat map tile provider.
             mOverlay = mMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
         }else{
+            // Change the data for the new one and clears the previous Tile Overlay (heat map)
             mProvider.setData(latLngList);
             mOverlay.clearTileCache();
         }
